da5bb6ca177db4334169b69886d0db69231a6203
chaudharyarsalan184-ai <chaudharyarsalan184@gmail.com>
2025-08-29 11:56:03 -0700

    Update moscow.php

--- moscow.php
+++ moscow.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>MiAmi - Reservations of flights</title>
+  <title>MiAmi - Fares Kart</title>
   <meta name="description" content="Disclaimer page for Hydra Travels INC. Learn about our terms of use, liability, and third-party links.">
   <link rel="stylesheet" href="style.css"> <!-- keep your existing CSS -->
 </head>
@@ -53,5 +53,6 @@
 <br>
 <h4>Local Cuisine</h4>
 <p>From borscht and pelmeni to beef stroganoff, blini, and savory pastries, Moscow’s culinary offerings are as rich and diverse as its history.
+
 
 </p>
