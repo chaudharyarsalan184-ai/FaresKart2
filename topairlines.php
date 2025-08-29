5f8409cb86b103644d86d7b04836dd8e216d804a
chaudharyarsalan184-ai <chaudharyarsalan184@gmail.com>
2025-08-29 11:59:21 -0700

    Update topairlines.php

--- topairlines.php
+++ topairlines.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title> Top Airlines Deals-Reservation Of Flights</title>
+    <title> Top Airlines Deals-Fares Kart</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <link rel="stylesheet" href="style.css">
 </head>
@@ -76,4 +76,5 @@
             </div>
         </div>
     </section>
+
 
